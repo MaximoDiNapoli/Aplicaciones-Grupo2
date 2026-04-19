5f9f75ce9106f15ca40bb7fdffc08932dc697663
Mohamed Mena <mohamed.mena@gmail.com>
2026-04-19 16:59:38 -0300

    Añadidos entidad de User, Repositorio, Service y Controller

---
+++ src/main/java/com/ecomerce/src/controller/UsuarioController.java
@@ -0,0 +1,69 @@
+package com.ecomerce.src.controller;
+
+import com.ecomerce.src.dto.UserRequest;
+import com.ecomerce.src.dto.UserResponse;
+import com.ecomerce.src.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UsuarioController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getUsers(
+        @RequestParam(required=false) String rol,
+        @RequestParam(required=false) String ciudad,
+        @RequestParam(required = false) String codigopostal
+    ) {
+        List<UserResponse> users = this.userService.getUsers(rol, ciudad, codigopostal);
+        return ResponseEntity.ok(users);
+    }
+
+    // GET /api/users/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        UserResponse user = this.userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    // PUT /api/users/{id}
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody UserRequest userDetails) {
+        UserResponse usuarioUpdated = this.userService.updateUser(id, userDetails);
+        return ResponseEntity.ok(usuarioUpdated);
+    }
+
+    // DELETE /api/users/{id}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        this.userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // --- Endpoints de Relaciones ---
+
+    @GetMapping("/{id}/compras")
+    public ResponseEntity<?> getUserCompras(@PathVariable Long id) {
+        // Lógica para traer compras del usuario
+        return ResponseEntity.ok("Lista de compras del usuario " + id);
+    }
+
+    @GetMapping("/{id}/carrito")
+    public ResponseEntity<?> getUserCarrito(@PathVariable Long id) {
+        // Lógica para traer el carrito actual
+        return ResponseEntity.ok("Carrito del usuario " + id);
+    }
+
+    @GetMapping("/{id}/direcciones")
+    public ResponseEntity<?> getUserDirecciones(@PathVariable Long id) {
+        // Lógica para traer direcciones
+        return ResponseEntity.ok("Direcciones del usuario " + id);
+    }
+}
